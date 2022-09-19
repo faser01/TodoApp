d1daec48a826e9c1eb25404df72f8ca5c07983a3
sergeevich ruslan <abishev2012@gmail.com>
2022-09-20 02:16:41 +0500

    Добавьте файлы проекта.

---
+++ Models/TodoModel.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel:INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; private set; }= DateTime.Now;
+
+		private bool _isDone;
+		private string _text;
+
+		
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnProperyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke (this, new PropertyChangedEventArgs(propertyName));
+			
+		}
+
+		public bool isDone
+		{
+			get { return  _isDone; }
+			set
+			{
+				if (_isDone == value)
+					return;
+				_isDone = value;
+				OnProperyChanged("IsDone");
+			}
+		}
+		public  string Text
+		{
+			get { return _text ; }
+			set 
+			{
+				if (_text == value)
+					return;
+				_text = value;
+				OnProperyChanged("Text");
+			}
+		}
+		
+
+
+
+	}
+}
